94c5eaeecad5b28433fe8bf9bd1842d107baea0e
LibertyHeise <109040678+LibertyHeise@users.noreply.github.com>
2023-01-09 11:09:51 -0600

    updates

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -10,7 +10,18 @@ d3.json("samples.json").then((data) => {
     var samples = data.samples;
     var metadata = data.metadata;
     var valdrop = samples.map(item => item.id)
-    .filter ((value,index,self) => self.indexOf (value)
+    .filter ((value,index,self) => self.indexOf (value) === index);
+
+//creating the dropdown menu
+
+valdrop.forEach(function(values) {
+    filter.append('option').text(values);
+});
+
+//samples based on input
+var resultArray = samples.filter(sampleObj => sampleObj.id ==samp);
+console.log(resultArray);
+
 
 
 
